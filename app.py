96c1c6960699ea0f349cf0538616cf977ca68b16
FelipeCortes123 <132774460+FelipeCortes123@users.noreply.github.com>
2024-01-26 08:58:26 -0600

    OPTMIZACION

--- app.py
+++ app.py
@@ -210,7 +210,6 @@ def downpdf():
                 pass
         if consultarpdf3 is not None:
             for i in consultarpdf3:
-                
                 b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17, b18, b19, b20, b21, b22, b23 = i
                 pdf_name = f"{contrato}_Caratula.pdf"
                 pdf = caratula_afiliado(pdf_name, b1, b2, b3, b4, b5, b6, b7, b8, b9, b10, b11, b12, b13, b14, b15, b16, b17, b18, b19, b20, b21, b22, b23,contrato)
