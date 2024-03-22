e54a9b4aacda96b4695ad83ed3ad594d449f70f0
serbox <juansebastian23072003@gmail.com>
2024-03-22 08:59:44 -0500

    rama inicial

--- prueba.py
+++ prueba.py
@@ -6,7 +6,7 @@ pdf_reader = PdfReader("Contrato_5A20293.pdf")
 for page_num in range(len(pdf_reader.pages)):
     pdf_writer.add_page(pdf_reader.pages[page_num])
 
-password = "1234"
+password = "1234" 
 pdf_writer.encrypt(password)
 
 with open("Contrato_5A20293.pdf", "wb") as file:
